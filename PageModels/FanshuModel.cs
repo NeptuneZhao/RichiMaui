78ce956b4ac57afa55664da4bcc48af4d548165e
HalfCooler <qq1836143240@outlook.com>
2025-07-24 18:05:57 +0800

    Complete most, bug will fix next at: 本场 entry

---
+++ PageModels/FanshuModel.cs
@@ -0,0 +1,72 @@
+﻿using System.ComponentModel;
+
+namespace RichiMaui.PageModels
+{
+    public class FanshuModel : INotifyPropertyChanged
+    {
+        object
+            zhuangOrXianjia,
+            zimoOrRong,
+            fan,
+            fu;
+
+        string benChang;
+        
+        public object ZhuangOrXianjia
+        {
+            get => zhuangOrXianjia;
+            set
+            {
+                zhuangOrXianjia = value;
+                OnPropertyChanged(nameof(ZhuangOrXianjia));
+            }
+        }
+
+        public object ZimoOrRong
+        {
+            get => zimoOrRong;
+            set
+            {
+                zimoOrRong = value;
+                OnPropertyChanged(nameof(ZimoOrRong));
+            }
+        }
+
+        public string BenChang
+        {
+            get => benChang;
+            set
+            {
+                benChang = value;
+                OnPropertyChanged(nameof(BenChang));
+            }
+        }
+
+        public object Fan
+        {
+            get => fan;
+            set
+            {
+                fan = value;
+                OnPropertyChanged(nameof(Fan));
+            }
+        }
+
+        public object Fu
+        {
+            get => fu;
+            set
+            {
+                fu = value;
+                OnPropertyChanged(nameof(Fu));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- Pages/Fanshu.xaml.cs
+++ Pages/Fanshu.xaml.cs
@@ -5,5 +5,11 @@ public partial class Fanshu : ContentPage
 	public Fanshu()
 	{
 		InitializeComponent();
+		BindingContext = new FanshuModel();
+	}
+
+    public void OnBenchangCompleted(object sender, EventArgs e)
+	{
+		((FanshuModel)BindingContext).BenChang = ((Editor)sender).Text;
 	}
 }
